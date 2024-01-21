deebab43812f9357d491738eaa8ca58fddffb6e4
Felihum <121069824+Felihum@users.noreply.github.com>
2024-01-20 22:47:23 -0300

    First Configuration

---
+++ src/components/NavBar/index.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./index.css"
+
+function NavBar(){
+    return(
+        <nav className="navbar">
+            <div className="cnt-logo">
+                <Link to="/">Home</Link>
+            </div>
+            <div className="cnt-mid">
+                <Link to="#">Biblioteca</Link>
+                <div className="btn-categoria">
+                    <Link to="#">Categorias</Link>
+                    <i id="icon-arrow" className='bx bx-chevron-down'></i>
+                </div>
+                
+                
+            </div>
+            <div className="cnt-end">
+                <Link to="#" id="btn-profile">Profile</Link>
+                <i id="icon-arrow" className='bx bx-chevron-down'></i>
+            </div>
+        </nav>        
+    )
+}
+
+export default NavBar;
---
+++ src/store/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './index.css'
+import NavBar from '../components/NavBar/index.tsx'
+
+function App() {
+  return (
+    <div>
+      <NavBar></NavBar>
+    </div>
+  )
+}
+
+export default App
