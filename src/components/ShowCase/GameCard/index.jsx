dbf18d1a39990cc1ce3f774b31f39f646bdae815
Felihum <121069824+Felihum@users.noreply.github.com>
2024-01-21 12:53:41 -0300

    Estilização do game card

--- src/components/ShowCase/GameCard/index.jsx
+++ src/components/ShowCase/GameCard/index.jsx
@@ -1,6 +1,16 @@
+import "./index.css"
+import img from '../../../images/god-ragnarok.jpg'
+
 function GameCard(){
     return(
         <div className="gameCard">
+            <div className="imgGame">
+                <img src={img} />
+            </div>
+            <div className="description">
+                <h1>God of War: Ragnarok</h1>
+                <p>$ 50,00</p>
+            </div>
             
         </div>
     )
--- src/components/ShowCase/GameShow/index.jsx
+++ src/components/ShowCase/GameShow/index.jsx
@@ -1,9 +1,10 @@
+import GameCard from "../GameCard/index.jsx";
 import "./index.css"
 
 function GameShow(){
     return(
         <div className="containerGeral">
-            
+            <GameCard></GameCard>
         </div>
     )
 }
