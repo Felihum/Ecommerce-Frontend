c766504548da4da35dfa087d7dd238d7d182fb32
Felihum <121069824+Felihum@users.noreply.github.com>
2024-01-22 11:59:35 -0300

    Card Button

--- src/components/ShowCase/GameCard/index.jsx
+++ src/components/ShowCase/GameCard/index.jsx
@@ -1,5 +1,7 @@
 import "./index.css"
 import img from '../../../images/god-ragnarok.jpg'
+import { IoCartOutline } from "react-icons/io5";
+import { FaPlus } from "react-icons/fa";
 
 function GameCard(){
     return(
@@ -10,6 +12,7 @@ function GameCard(){
             <div className="description">
                 <h1 className="title">God of War: Ragnarok</h1>
                 <p className="price">$ 50,00</p>
+                <button className="btnCarrinho"><IoCartOutline id="icon-cart" /><FaPlus id="icon-plus" /></button>
             </div> 
         </div>
     )
